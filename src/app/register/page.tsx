70caa74ecee511dcb7cf3a58173ba5dc45faf3ad
usman ali <usmanali477275@gmail.com>
2024-11-18 00:00:50 +0500

    Registeration basic setup

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "@/featuers/Register/components/RegisterForm";
+import React from "react";
+
+function Register() {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default Register;
