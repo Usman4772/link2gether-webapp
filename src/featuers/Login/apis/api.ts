62978a8f525d2cca509f92a0769fc1602a6c76ab
usman ali <usmanali477275@gmail.com>
2025-02-04 15:10:53 +0500

    fixes

--- src/featuers/Login/apis/api.ts
+++ src/featuers/Login/apis/api.ts
@@ -1,5 +1,9 @@
 import httpInstance from "@/utils/config/axios";
+import axios from "axios";
 
 export function LoginAPI(data: any) {
-  return httpInstance.post("/auth/login", data);
+  return axios.post(
+    "https://link2gether-webapp.vercel.app/api/auth/login",
+    data
+  );
 }
