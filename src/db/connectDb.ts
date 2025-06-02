d79582d1d2eb8336dfb2e0db18a8421b0da48931
usman ali <usmanali477275@gmail.com>
2025-06-02 14:23:11 +0500

    Mongodb connection fix

--- src/db/connectDb.ts
+++ src/db/connectDb.ts
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+let isConnected = false;
 
 export async function connectDb() {
+  if (isConnected) return;
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(
+      "mongodb+srv://usmanali477275:8HUZ5W0jdAiB4EV8@cluster0.icbe8gd.mongodb.net/link-to-gether?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    isConnected = true;
     return true;
   } catch (error) {
     console.log("Db not connected", error);
